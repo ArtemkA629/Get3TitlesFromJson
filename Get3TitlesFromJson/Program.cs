f06c7624d58a4d7e63204d906cfe2eb00e434778
ArtemkA629 <deffing9@mail.ru>
2023-02-10 20:55:40 +0300

    1 версия

---
+++ Get3TitlesFromJson/Program.cs
@@ -0,0 +1,20 @@
+﻿int titlesNumber = 3;
+string json = await GetJsonString();
+string[] titles = GetTitles(json);
+for (int i = 0; i < titlesNumber; i++)
+    Console.WriteLine(titles[new Random().Next(0, titles.Length)]);
+
+static async Task<string> GetJsonString()
+{
+    using (HttpClient httpClient = new HttpClient())
+        return await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+}
+
+static string[] GetTitles(string json)
+{
+    string[] titleLines = json.Split('\n')
+        .Where(x => x.Contains("title"))
+        .ToArray();
+    int titleIndex = 3;
+    return titleLines.Select(x => x!.Split("\"")[titleIndex]).ToArray();
+}
